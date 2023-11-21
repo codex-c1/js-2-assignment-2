19a478f350cb52686b4de73b7b06e6c073723b2d
JeremieAgee <you@example.com>
2023-11-20 21:38:10 -0600

    Added Buttons and inputs

--- script.js
+++ script.js
@@ -9,18 +9,36 @@ var aliceSays, bobSays, carolSays, danSays, ellieSays, frankSays, harrySays, ivy
 
 
 // this variable is set to a string.
-let theMessage = "Kathy loves Larry";
-
-aliceSays = theMessage;
-bobSays = aliceSays;
-carolSays = bobSays;
-danSays = carolSays;
-ellieSays = danSays;
-frankSays = ellieSays;
-gregSays = frankSays;
-harrySays = gregSays;
-ivySays = harrySays;
-jackSays = ivySays;
-// call setNames from 'messages.js'
-// to set the messages in the DOM.
 setMessages();
+function setAll(){ 
+    let theMessage = "Kathy loves Larry";
+    aliceSays = theMessage;
+    bobSays = aliceSays;
+    carolSays = bobSays;
+    danSays = carolSays;
+    ellieSays = danSays;
+    frankSays = ellieSays;
+    gregSays = frankSays;
+    harrySays = gregSays;
+    ivySays = harrySays;
+    jackSays = ivySays;
+    setMessages();
+}
+let allBtn = document.querySelector("#all-btn");
+allBtn.onclick = setAll;// call setNames from 'messages.js'
+// to set the messages in the DOM.
+function setAlice(){
+    let alice = document.querySelector("#alice-input").value;
+    aliceSays = alice;
+    setMessages();
+}
+aliceBtn = document.querySelector("#alice-btn");
+aliceBtn.onclick = setAlice;
+function setBob(){
+    let bob = document.querySelector("#bob-input").value;
+    bobSays = bob;
+    setMessages();
+}
+bobBtn = document.querySelector("#bob-btn");
+bobBtn.onclick = setBob;
+
