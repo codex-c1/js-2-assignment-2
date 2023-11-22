35a344f0177bf59578066066ea6ba29dc0694166
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2023-11-21 21:09:43 -0600

    Finished the flow in script.

--- script-2.js
+++ script-2.js
@@ -1,23 +1,45 @@
-
 // It's a good idea to declare all your variables.
-// This will let our JS linter help us find 
+// This will let our JS linter help us find
 // spelling errors among other things.
 // Declaring with 'var' will make them global
 // variables.
 
-var alicesBulletin, bobsBulletin, carolsBulletin, dansBulletin, elliesBulletin, franksBulletin, gregsBulletin, harrysBulletin, ivysBulletin, jacksBulletin
+var alicesBulletin,
+  bobsBulletin,
+  carolsBulletin,
+  dansBulletin,
+  elliesBulletin,
+  franksBulletin,
+  gregsBulletin,
+  harrysBulletin,
+  ivysBulletin,
+  jacksBulletin;
 
 let schoolBulletin;
 
 // this variable is set to an object
 schoolBulletin = {
-  message: "Cougars Win!"
-}
+  message: "Cougars Win!",
+};
+
+alicesBulletin = {
+  message: "Sox Win!",
+};
 
 alicesBulletin = schoolBulletin;
 bobsBulletin = alicesBulletin;
 carolsBulletin = bobsBulletin;
+dansBulletin = carolsBulletin;
+elliesBulletin = dansBulletin;
+franksBulletin = elliesBulletin;
+gregsBulletin = franksBulletin;
+harrysBulletin = gregsBulletin;
+ivysBulletin = harrysBulletin;
+jacksBulletin = ivysBulletin;
 
+carolsBulletin.message = "Ghosts Win!";
+
+// Changed carlosBulletine message and it changed all of them/
 
 // call setNames from 'messages.js'
 // to set the messages in the DOM.
