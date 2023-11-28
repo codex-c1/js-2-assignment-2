bd669050182844c261bdb15badeac7b9cb314f3c
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2023-11-27 18:48:47 -0600

    added setBulletins in script to get to work.

--- script-2.js
+++ script-2.js
@@ -42,16 +42,17 @@ carolsBulletin.message = "Ghosts Win!";
 gregsBulletin.message = "Saints win... Finally!";
 // changed gregs message and it changed everyones message.
 
-// function setNewMessage() {
-//   let inputElm, message;
-//   inputElm = document.querySelector("#new-message");
-//   message = inputElm.value;
-//   schoolBulletin.message = message;
-// }
+function setNewMessage() {
+  let inputElm, message;
+  inputElm = document.querySelector("#new-message");
+  message = inputElm.value;
+  schoolBulletin.message = message;
+  setBulletins();
+}
 
-// let msgBtn;
-// msgBtn = document.querySelector("#msg-button");
-// msgBtn.onclick = setNewMessage;
+let msgBtn;
+msgBtn = document.querySelector("#msg-button");
+msgBtn.onclick = setNewMessage;
 
 // call setNames from 'messages.js'
 // to set the messages in the DOM.
